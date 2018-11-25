b93ce51c4e1ea2931fa168d0b98862b713c7a469
cummings-chris <chris@cummings.tech>
2018-11-24 21:38:32 -0600

    added configs, MariaDB

--- bin/air_quality.py
+++ bin/air_quality.py
@@ -1,24 +1,52 @@
 from AM2315 import AM2315
+import mysql.connector as mariadb
 import time
+import os
+from configparser import ConfigParser
+
+file_path = os.path.dirname(__file__)
+if file_path != "":
+    os.chdir(file_path)
+
+parser = ConfigParser()
+parser.read('../config/aqpi_config.py')
+
+db_user = parser.get('mysql', 'user')
+db_password = parser.get('mysql', 'password')
+db_host = parser.get('mysql', 'host')
+db_name = parser.get('mysql', 'name')
+
+mariadb_connection = mariadb.connect(\
+    user=db_user, password=db_password, host=db_host, database=db_name, autocommit=True\
+)
+cursor = mariadb_connection.cursor(buffered=True)
 
 sensor=AM2315()
 
+
 while 1 == 1:
-#Set variables to None so that we don't have false data if there is an issuew with the sensor.
+    #Set variables to None so that we don't have false data if there is an issue with the sensor.
     current_time = None
     temp = None
     rh = None
-    pm = None
+    pm2_5 = None
+    pm10 = None
     so2 = None
     no2 = None
     o3 = None
     co = None
 
-    current_time = time.time()
+    current_time = time.strftime('%Y-%m-%d %H:%M:%S')
     temp = sensor.temperature()
     rh = sensor.humidity()
 
-    print([current_time, temp, rh])
+    cursor.execute(\
+        "INSERT INTO air_quality (\
+            `current_time`, `temp`, `rh`, `pm2_5`, `pm10`, `so2`, `no2`, `o3`, `co`) \
+            VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)",\
+            (current_time, temp, rh, pm2_5, pm10, so2, no2, o3, co)\
+        )
+
     time.sleep(5)
 
 
---
+++ config/aqpi_config.py
@@ -0,0 +1,5 @@
+[mysql]
+host= localhost
+user= aqpi
+password = Eew2vzjf9QBQv6g5
+name = AQpi
